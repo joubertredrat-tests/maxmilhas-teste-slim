77d3872fd7a501c701774ac7401a6b6ef7c75ac8
Joubert RedRat <me+github@redrat.com.br>
2018-04-15 00:45:06 -0300

    Update dependences

--- src/routes.php
+++ src/routes.php
@@ -1,18 +1,6 @@
 <?php
 
-use Slim\Http\Request;
-use Slim\Http\Response;
-
 // Routes
-
-//$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-//    // Sample log message
-//    $this->logger->info("Slim-Skeleton '/' route");
-//
-//    // Render index view
-//    return $this->renderer->render($response, 'index.phtml', $args);
-//});
-
 $app->get(
     '/form',
     'App\Controller\FormController:showAction'
